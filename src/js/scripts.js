555726cc8b6c9b95ac6a74e6b55a15219e18ccef
Eduardo Leite <94940400+oeduardodev@users.noreply.github.com>
2023-06-08 19:12:24 -0300

    atualizando responsividade

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,3 +9,31 @@
     }
   }
   
+  document.addEventListener('DOMContentLoaded', function() {
+    function carousel() {
+      const carouselContainer = document.querySelector('.carousel-container');
+      const carousel = document.querySelector('.carousel');
+
+      const containerWidth = carouselContainer.offsetWidth;
+      const carouselWidth = carousel.offsetWidth;
+
+      let position = 0;
+      let velocity = .80;
+
+      function animate() {
+        position -= velocity;
+
+        if (position < -carouselWidth) {
+          position = 0;
+        }
+
+        carousel.style.transform = `translateX(${position}rem)`;
+
+        requestAnimationFrame(animate);
+      }
+
+      animate();
+    }
+
+    carousel();
+  });
